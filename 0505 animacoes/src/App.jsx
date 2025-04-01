3d937a5859819ac1f6b7d3e41a0004670550a619
Gabriel Linhares <linhares.gs96@gmail.com>
2025-04-01 20:53:35 -0300

    0505 animacoes

--- 0505 animacoes/src/App.jsx
+++ 0505 animacoes/src/App.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import './App.css';
+import Produto from './Produto.jsx';
 
 const App = () => {
-  return <div>App React</div>;
+  const [ativar, setAtivar] = React.useState(false);
+
+  return (
+    <>
+      <button onClick={() => setAtivar(!ativar)}>Ativar</button>
+      {ativar && <Produto></Produto>}
+    </>
+  );
 };
 
 export default App;
---
+++ 0505 animacoes/src/Produto.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Produto = () => {
+  return (
+    <div className="animeLeft">
+      <h1>Produto</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores
+        atque accusantium consequatur obcaecati, dolores sit at, nostrum numquam
+        doloribus harum nemo ipsa minus maiores repellendus, rem ducimus sed!
+        Ad, voluptatum!
+      </p>
+    </div>
+  );
+};
+
+export default Produto;
