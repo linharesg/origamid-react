b461ee91baedddb8ca42972df83b3f9201cf2ad8
Gabriel Linhares <linhares.gs96@gmail.com>
2025-04-03 21:11:54 -0300

    0505 animacao slide

--- 0505 animacoes/src/App.jsx
+++ 0505 animacoes/src/App.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import './App.css';
 import Produto from './Produto.jsx';
+import Slide from './Slide.jsx';
 
 const App = () => {
+
+  const slides = [
+    {
+      id: "slide1",
+      text: "Slide 1"
+    },
+    {
+      id: "slide2",
+      text: "Slide 2"
+    },
+    {
+      id: "slide3",
+      text: "Slide 3"
+    },
+  ]
+  
   const [ativar, setAtivar] = React.useState(false);
 
   return (
     <>
       <button onClick={() => setAtivar(!ativar)}>Ativar</button>
       {ativar && <Produto></Produto>}
+      <Slide slides={slides} />
     </>
   );
 };
---
+++ 0505 animacoes/src/Slide.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import styles from './Slide.module.css';
+
+const Slide = ({ slides }) => {
+  const [active, setActive] = React.useState(0);
+  const [position, setPosition] = React.useState(0);
+  const contentRef = React.useRef();
+
+  React.useEffect(() => {
+    const { width } = contentRef.current.getBoundingClientRect();
+    setPosition(-(active * width));
+  }, [active]);
+
+  function slideNext() {
+    if (active < slides.length - 1) setActive(active + 1);
+  }
+
+  function slidePrev() {
+    if (active > 0) setActive(active - 1);
+  }
+
+  return (
+    <section className={styles.container}>
+      <div
+        ref={contentRef}
+        className={styles.content}
+        style={{ transform: `translateX(${position}px)` }}
+      >
+        {slides.map((slide) => (
+          <div key={slide.id} className={styles.item}>
+            {slide.text}
+          </div>
+        ))}
+      </div>
+      <nav className={styles.nav}>
+        <button disabled={!(active > 0)} onClick={slidePrev}>
+          Anterior
+        </button>
+        <button disabled={!(active < slides.length - 1)} onClick={slideNext}>
+          Próximo
+        </button>
+      </nav>
+    </section>
+  );
+};
+
+export default Slide;
