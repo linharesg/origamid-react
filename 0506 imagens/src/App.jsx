ed57c4f08bb29ff94fd8d01226fa2a7ac99a8b8d
Gabriel Linhares <linhares.gs96@gmail.com>
2025-04-05 13:07:45 -0300

    0506 imagens

--- 0506 imagens/src/App.jsx
+++ 0506 imagens/src/App.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import foto from './img/foto.png';
+import './App.css';
+import Piggy from './img/piggy-bank.svg?react';
+import PiggySvg from './img/PiggySvg';
 
 const App = () => {
-  return <div>App React</div>;
+  return (
+    <div>
+      <p className="fundo"></p>
+      <Piggy />
+      <PiggySvg color={'red'} />
+      <img src={foto} alt="foto" />
+    </div>
+  );
 };
 
 export default App;
